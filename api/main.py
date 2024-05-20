8a93451903172de3ebe2f10b1ddb0fc532d49cb2
alex.lin <alex@mac.local>
2024-05-20 21:08:28 +0800

    Fix: logging config

--- api/main.py
+++ api/main.py
@@ -1,9 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from .router import task
-import logging
-
-
-logging.basicConfig(level=logging.WARN, format="%(asctime)s %(levelname)s: %(message)s")
+import logging, sys
 
 app = FastAPI()
+
+# logging config
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+log_formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setFormatter(log_formatter)
+logger.addHandler(stream_handler)
+
+
+@app.middleware("http")
+async def logger_middleware(request: Request, call_next):
+    client = request.client
+    method = request.method
+    url = request.url
+    logger.info(f"client: {client}, method: {method}, url: {url}")
+    response = await call_next(request)
+    return response
+
+
 app.include_router(prefix="/task", tags=["task"], router=task.router)
